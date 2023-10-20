a8f3e910be25ef856077071fe499a976c37ad810
Tulio Pinto Neto <tulio.neto@startaideia.com.br>
2023-10-20 20:25:18 +0100

    feat: add analytics tag to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,26 @@ import { AboutUs as Team } from "src/containers/Team/index";
 import { Services } from "containers/Services";
 import { Testimonials } from "src/containers/Testimonials";
 import { Footer } from "src/containers/Footer";
+import Script from "next/script";
+
+function AnalyticsTag() {
+  return (
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-NXJWMD9YVP"
+      ></Script>
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-NXJWMD9YVP');
+        `}
+      </Script>
+    </>
+  );
+}
 
 export default function Home() {
   return (
@@ -19,6 +39,7 @@ export default function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <AnalyticsTag />
       <main>
         <div itemScope itemType="http://schema.org/Organization">
           <HomeMain />
