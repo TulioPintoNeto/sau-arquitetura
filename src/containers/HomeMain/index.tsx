e3a5f3de6b45ddd45c37e57c86ee0a4a336948b1
Tulio Pinto Neto <tulio.neto@startaideia.com.br>
2023-10-21 22:32:55 +0100

    feat: setup playwright and visual regression tests for home page]

--- src/containers/HomeMain/index.tsx
+++ src/containers/HomeMain/index.tsx
@@ -1,14 +1,17 @@
 import Container from "components/Container";
-import { HeaderSection } from 'components/HeaderSection/index';
+import { HeaderSection } from "components/HeaderSection/index";
 import styles from "./styles.module.scss";
+import { useHomeMainHeight } from "src/hooks/useHomeMainHeight";
 
 const HomeMain = () => {
+  const height = useHomeMainHeight();
+
   return (
-    <div className={styles.background}>
-      <div className={styles.mainSectionBox}>
+    <div className={styles.background} style={{ height }}>
+      <div className={styles.mainSectionBox} style={{ height }}>
         <Container>
           <div className={styles.constrainedBox}>
-            <HeaderSection 
+            <HeaderSection
               main
               text="Projetando e realizando sonhos"
               title="Arquitetura e Construção Civil em Pelotas"
