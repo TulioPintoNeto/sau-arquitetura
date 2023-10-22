29863f09f16224b02dee09b6d85ff5646c09db7a
Tulio Pinto Neto <tulio.neto@startaideia.com.br>
2023-10-22 20:59:09 +0100

    chore: update snapshots and add playwright to CI on PR to master

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,4 +38,10 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
   ],
+  webServer: {
+    command: "npm run build && npm run start",
+    url: "http://127.0.0.1:3000",
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
 });
