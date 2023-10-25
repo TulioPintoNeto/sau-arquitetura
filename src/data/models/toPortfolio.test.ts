ff526dd3d238f6c6189e4bf7d3dfd38ef5f4676b
Tulio Pinto Neto <tulio.neto@startaideia.com.br>
2023-10-25 16:22:16 +0100

    test/chore: add test to toPortfolio and add pre-commit for husky. Also add jest

---
+++ src/data/models/toPortfolio.test.ts
@@ -0,0 +1,68 @@
+import { toPortfolio } from "./toPortfolio";
+
+const input = {
+  items: [
+    {
+      fields: {
+        name: "Cozinha - Rafael Dias Mazza (2)",
+        photos: [
+          {
+            sys: {
+              id: "4gjWcobT7JnaBH7IDeCOpn",
+            },
+          },
+        ],
+      },
+    },
+    {
+      fields: {
+        name: "Cozinha - Rafael Dias Mazza",
+        photos: [
+          {
+            sys: {
+              id: "4gjWcobT7JnaBH7IDeCOpn",
+            },
+          },
+        ],
+      },
+    },
+  ],
+  includes: {
+    Asset: [
+      {
+        sys: {
+          id: "4gjWcobT7JnaBH7IDeCOpn",
+        },
+        fields: {
+          description: "Alguma descrição",
+          file: {
+            url: "//images.ctfassets.net/b3ruagn4bm0g/4gjWcobT7JnaBH7IDeCOpn/6f9eeadda36a397c42fe28181c66b9a7/b_730_0ca80fb8-8702-47c7-b056-267a697d52f0.jpg",
+          },
+        },
+      },
+    ],
+  },
+};
+
+const output = [
+  {
+    name: "Cozinha - Rafael Dias Mazza (2)",
+    photo: {
+      alt: "Alguma descrição",
+      url: "//images.ctfassets.net/b3ruagn4bm0g/4gjWcobT7JnaBH7IDeCOpn/6f9eeadda36a397c42fe28181c66b9a7/b_730_0ca80fb8-8702-47c7-b056-267a697d52f0.jpg",
+    }
+  },
+  {
+    name: "Cozinha - Rafael Dias Mazza",
+    photo: {
+      alt: "Alguma descrição",
+      url: "//images.ctfassets.net/b3ruagn4bm0g/4gjWcobT7JnaBH7IDeCOpn/6f9eeadda36a397c42fe28181c66b9a7/b_730_0ca80fb8-8702-47c7-b056-267a697d52f0.jpg",
+    }
+  }
+];
+
+test('toPortfolio model', () => {
+  const result = toPortfolio(input);
+
+  expect(result).toStrictEqual(output);
+});
