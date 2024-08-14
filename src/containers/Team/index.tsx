f0170d820775b88780001deeab3dab1c6647d6e5
Tulio Pinto Neto <tulio_neto_2@hotmail.com>
2024-08-14 12:42:47 +0100

    feat: remove fabricio from team section

--- src/containers/Team/index.tsx
+++ src/containers/Team/index.tsx
@@ -10,7 +10,11 @@ export function AboutUs() {
       <Container verticalPadding>
         <Row className={styles.row}>
           <div>
-            <HeaderSection text="Quem é a" title="SAU Arquitetura" titleItemProp="name"/>
+            <HeaderSection
+              text="Quem é a"
+              title="SAU Arquitetura"
+              titleItemProp="name"
+            />
           </div>
           <div>
             <PersonSection
@@ -20,18 +24,8 @@ export function AboutUs() {
                 "Arquiteto e Urbanista formado pela Universidade Católica de Pelotas, sócio fundador da SAU Arquitetura Ltda e responsável pela área comercial e de projetos.",
                 "Possui expertise em aprovação de projetos em órgãos públicos e experiência em elaboração dos mais diversos trabalhos, desde alto padrão em condomínios fechados à obras públicas licitadas.",
               ]}
-              name="Pablo Isnardi" />
-          </div>
-          <div>
-            <PersonSection
-              alt="Na foto preto e branca, Fabrício dos Santos, um homem negro usando uma camisa social branca e óculos"
-              src="/fabricio.png"
-              descriptions={[
-                "Arquiteto e Urbanista formado pela Universidade Católica de Pelotas, sócio fundador da SAU Arquitetura Ltda e responsável pela área orçamentária e de obras.",
-                "No mercado da construção civil há mais de dez anos trabalhou em grandes construtoras na cidade, além de empreitar obras particulares em condomínios fechados de Pelotas como Lagos de São Gonçalo e Alphaville.",
-              ]}
-              name="Fabrício dos Santos"
-              reversed />
+              name="Pablo Isnardi"
+            />
           </div>
         </Row>
       </Container>
